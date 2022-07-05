ebdce80821364fa97cb1c6e7fd392867214f49a1
Raphael Marques <rmarquescomputadores@gmail.com>
2022-07-05 10:35:12 -0300

    Update
    Atualizei funções hover, aplicando ternario para modificar a classe conforme o texto

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,23 +4,13 @@ import styles from './Main.module.css';
 import { FaWhatsapp, FaLinkedinIn, FaMailBulk, FaGithubSquare } from 'react-icons/fa'
 
 function Main() {
-    function backgroundWhatsapp(e) {
-        e.target.style.background = "#25D366"
-    }
-    function backgroundLinkedin(e) {
-        e.target.style.backgroundColor = "#0077b5"
-    }
-    function backgroundGithub(e) {
-        e.target.style.background = "#272b33"
-    }
-    function backgroundEmail(e) {
-        e.target.style.background = "#8B0000"
-    }
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 <div>
-                    <section></section>
+                    <section>
+                    {/* Foto de perfil */}
+                    </section>
                     <h1>Raphael Marques</h1>
                 </div>
                 <div>
@@ -32,10 +22,11 @@ function Main() {
                 </div>
             </header>
             <main>
-                <Span href="https://api.whatsapp.com/send/?phone=5531991881701&text&app_absent=0" text='Whatsapp' icon={<FaWhatsapp />} color={backgroundWhatsapp} />
-                <Span href="https://www.linkedin.com/in/raphhael-m/" text='Linkedin' icon={<FaLinkedinIn />} color={backgroundLinkedin} />
-                <Span href="https://github.com/raphhaelm" text='GitHub' icon={<FaGithubSquare />} color={backgroundGithub} />
-                <Span href="mailto:rmarquescomputadores@gmail.com" text='E-mail' icon={<FaMailBulk />} color={backgroundEmail} />
+                
+                <Span href="https://www.linkedin.com/in/raphhael-m/" text='Linkedin' icon={<FaLinkedinIn />} />
+                <Span href="https://github.com/raphhaelm" text='GitHub' icon={<FaGithubSquare />} />
+                <Span href="https://api.whatsapp.com/send/?phone=5531991881701&text&app_absent=0" text='Whatsapp' icon={<FaWhatsapp />}  />
+                <Span href="mailto:rmarquescomputadores@gmail.com" text='E-mail' icon={<FaMailBulk />}  />
             </main>
         </div>
     )
--- src/components/Span/Span.jsx
+++ src/components/Span/Span.jsx
@@ -5,7 +5,8 @@ import { FaWhatsapp, FaLinkedinIn, FaMailBulk, FaGithubSquare } from 'react-icon
 function Span(props) {
 
     return (
-        <div onMouseOver={props.color} onMouseOut={props.mouseOut} className={styles.container} >
+        <main main className={styles.container}>
+        <div onMouseOver={props.color} onMouseOut={props.mouseOut} className={ props.text == 'Whatsapp' ? styles.containerWhatsapp : props.text == 'Linkedin' ? styles.containerLinkedin : props.text == 'GitHub' ? styles.containerGithub : props.text == 'E-mail' ? styles.containerEmail : styles.container } >
             <div  className={styles.text} >
                 <a href={props.href}>
                     <span>
@@ -20,6 +21,7 @@ function Span(props) {
 
 
         </div>
+        </main>
 
     )
 }
